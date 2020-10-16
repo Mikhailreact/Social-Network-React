0c5387cc9487534d4b0df331c8084628136eda01
Your Name <migrjsts@gmail.com>
2020-10-16 10:58:56 +0300

    first commit

---
+++ src/componens/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+	return (
+		<div className="content">
+			<div>
+				<img src="https://www.iexpats.com/wp-content/uploads/2017/11/paradise-island.jpg" />
+			</div>
+			<div>
+				Ava + description
+		</div>
+			<div>
+				My post
+			<div>
+					New post
+			</div>
+			</div>
+			<div>
+				<div>
+					post1
+			</div>
+				<div>
+					post2
+			</div>
+			</div>
+		</div>
+
+	)
+}
+
+export default Profile;
