9107ae0c132ac2fa354ae190312c12d29983dd89
Grinchenko Mikhail <grinchenco.01@mail.ru>
2020-12-19 09:32:54 +0300

    Task37 store,state

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,55 +1,62 @@
-let rerenderEntireTree = () => {
-    console.log('State is change');
-}
+let store = {
+    _state:  {
+        profilePage: {
+            posts: [{id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+                {id: 2, message: 'blabla', likesCount: 1100},
+                {id: 2, message: 'dada', likesCount: 11},],
+            newPostText: '',
 
-
-let state = {
-    profilePage: {
-        posts: [{id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 2, message: 'It\'s my first post', likesCount: 11},
-            {id: 2, message: 'blabla', likesCount: 1100},
-            {id: 2, message: 'dada', likesCount: 11},],
-        newPostText: '',
-
+        },
+        dialogsPage: {
+            dialogs: [{id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'}],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'yo'},
+                {id: 4, message: 'yo'},
+                {id: 5, message: 'yo'}
+            ]
+        },
+        sidebar: {}
     },
-    dialogsPage: {
-        dialogs: [{id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'}],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'yo'},
-            {id: 4, message: 'yo'},
-            {id: 5, message: 'yo'}
-        ]
+    getState(){
+        return this._state;
     },
-    sidebar: {}
+    _callSubscriber()  {
+        console.log('State is change');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; // Мы переприсвоили действие функции и теперь она вызывает запуск rerenderEntireTree в index.js , котороя в свою очередь запускает отррисовку App.
+    },
+
+
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; // Мы переприсвоили действие функции и теперь она вызывает запуск rerenderEntireTree в index.js , котороя в свою очередь запускает отррисовку App.
-}
 // observer == addEventListener
 // observer - наблюдатель
 
-export default state;
+window.store = store;
+
+export default store;
