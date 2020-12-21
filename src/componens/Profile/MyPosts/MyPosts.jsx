170803b0a4faa3213b622cc31910a284eb3adb25
Grinchenko Mikhail <grinchenco.01@mail.ru>
2020-12-21 12:05:58 +0300

    Task41  reducer (почти redux)

--- src/componens/Profile/MyPosts/MyPosts.jsx
+++ src/componens/Profile/MyPosts/MyPosts.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Posts from "./Post/Post";
-import state, {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
+import state, {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
 
