45dbe610f3f2964f3aea781cc32f819fb20d5367
Grinchenko Mikhail <grinchenco.01@mail.ru>
2020-12-25 15:25:00 +0300

    Task43 Everything up-to-date

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import {BrowserRouter, Route} from "react-router-dom";
 import Music from "./componens/Music/music";
 import News from "./componens/News/news";
 import Settings from "./componens/Settings/settings";
+import DialogsContainer from "./componens/Dialogs/DialogsContainer";
 
 
 const App = (props) => {
-
+debugger
     return (
         <BrowserRouter>
             <div className="app-wrapper">
@@ -19,11 +20,12 @@ const App = (props) => {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path="/dialogs"
-                           render={() => <Dialogs store={props.store}/>}/>
+                           render={() => <DialogsContainer store={props.store}/>}/>
                     <Route path="/profile"
                            render={() => <Profile
-                               profilePage={props.state.profilePage}
-                               dispatch={props.dispatch}
+                               store={props.store}
+                               /*profilePage={props.state.profilePage}
+                               dispatch={props.dispatch}*/
                            />}/>
                     <Route path="/Music" render={() => <Music/>}/>
                     <Route path="/News" render={() => <News/>}/>
--- src/componens/Profile/MyPosts/MyPosts.jsx
+++ src/componens/Profile/MyPosts/MyPosts.jsx
@@ -4,7 +4,6 @@ import Posts from "./Post/Post";
 import state, {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 
 const MyPosts = (props) => {
-
     let postsElements =
         props.posts.map( p => <Posts massage={p.message} likesCount={p.likesCount}/>);
 
@@ -12,17 +11,13 @@ const MyPosts = (props) => {
 
 window.state = state;
 
-    let addPost = () => {
-        //props.addPost();
-        props.dispatch(addPostActionCreator());
+    let onAddPost = () => {
+        props.addPost();
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        //props.updateNewPostText(text);
-        //let action = { type: 'UPDATE-NEW-POST-TEXT', newText : text };
-        let action = updateNewPostTextActionCreator(text);
-        props.dispatch(action);
+        props.updateNewPostText(text);
     }
 
     return (
@@ -33,7 +28,7 @@ window.state = state;
                     <textarea onChange={ onPostChange } ref={newPostElement} value={props.newPostText}/>
                 </div>
                 <div>
-                    <button onClick={ addPost }>Add post</button>
+                    <button onClick={ onAddPost }>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
